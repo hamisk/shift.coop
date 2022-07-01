fe8a16ac8759cde3444f838eab33fd7cf45fffa6
hamisk <hs.khera@icloud.com>
2022-06-30 19:06:47 -0700

    Edit home

--- client/src/Pages/Home/Home.tsx
+++ client/src/Pages/Home/Home.tsx
@@ -1,17 +1,19 @@
-import React from 'react'
-import './Home.scss'
+import React from 'react';
+import './Home.scss';
 
 const Home: React.FC = () => {
-    return (
-        <section className='home'>
-            <div className="home__background"></div>
-            <div className="home__overlay">
-                <h1 className="home__title">Shift.coop</h1>
-                <p className="home__hero-text">content on hero</p>
-                <p className="home__hero-text">background image or live video</p>
-            </div>
-        </section>
-    )
-}
+  return (
+    <section className='home'>
+      <div className='home__column-left'></div>
+      <div className='home__column-right'></div>
+      {/* <div className='home__background'></div>
+      <div className='home__overlay'>
+        <h1 className='home__title'>Shift.coop</h1>
+        <p className='home__hero-text'>content on hero</p>
+        <p className='home__hero-text'>background image or live video</p>
+      </div> */}
+    </section>
+  );
+};
 
-export default Home
+export default Home;
