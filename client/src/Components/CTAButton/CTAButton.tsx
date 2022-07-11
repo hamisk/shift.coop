7e22f79651ebe53193eeb2681e0c8ac3ab3fab86
hamisk <hs.khera@icloud.com>
2022-07-10 18:05:19 -0700

    Style header, add logo

--- client/src/Components/CTAButton/CTAButton.tsx
+++ client/src/Components/CTAButton/CTAButton.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import './CTAButton.scss'
+import React from 'react';
+import './CTAButton.scss';
 
 interface Props {
-    buttonText: string;
-    bgColour?: string;
-    link?: string
+  buttonText: string;
+  bgColour?: string;
+  link?: string;
 }
 
 const CTAButton: React.FC<Props> = ({ buttonText, bgColour, link }) => {
   return (
     <button className='ctabutton'>
-        <p className="ctabutton__text">{buttonText}</p>
+      <p className='ctabutton__text'>{buttonText}</p>
     </button>
-  )
-}
+  );
+};
 
-export default CTAButton
+export default CTAButton;
--- client/src/Components/Header/Header.tsx
+++ client/src/Components/Header/Header.tsx
@@ -1,27 +1,34 @@
-import React from 'react'
-import CTAButton from '../CTAButton/CTAButton'
-import './Header.scss'
+import React from 'react';
+import CTAButton from '../CTAButton/CTAButton';
+import './Header.scss';
+import shiftLogo from '../../assets/logos/medium/Shift_RGB_medium.png';
 
 const Header: React.FC = () => {
   return (
     <header className='header'>
-        <div className="header__logo-nav">
-            <h2 className="header__logo">Shift.coop</h2>
-            <nav className="header__nav">
-                <ul className="header__links">
-                    <li className="header__nav-link">features</li>
-                    <li className="header__nav-link">pricing</li>
-                    <li className="header__nav-link">community</li>
-                    <li className="header__nav-link">support</li>
-                </ul>
-            </nav>
-        </div>
-        <div className="header__login-signup">
-            <CTAButton buttonText='Login' />
-            <CTAButton buttonText='Register' />
-        </div>
+      <div className='header__logo-nav'>
+        {/* <h2 className='header__logo'>Shift.coop</h2> */}
+        <img className='header__logo' src={shiftLogo} alt='Shift logo' />
+        <nav className='header__nav'>
+          <ul className='header__links'>
+            {/* <li className='header__nav-link'>Home</li> */}
+            <li className='header__nav-link'>Services</li>
+            <li className='header__nav-link'>About Us</li>
+            {/* <li className='header__nav-link'>Vision</li> */}
+            {/* <li className='header__nav-link'>Team</li> */}
+            {/* <li className='header__nav-link'>Partners</li> */}
+            {/* <li className='header__nav-link'>Media</li> */}
+            <li className='header__nav-link'>Contact</li>
+            {/* <li className='header__nav-link'>Blog</li> */}
+          </ul>
+        </nav>
+      </div>
+      <div className='header__login-signup'>
+        <CTAButton buttonText='Get a Quote' />
+        <CTAButton buttonText='Login' />
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
